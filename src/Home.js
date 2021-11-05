c7739018b4adba51156c405dca5a3248bd8e9dec
Mary Baker <meb601@gmail.com>
2021-11-05 18:23:08 -0400

    Remove create-react-app boilerplate

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import logo from './turkey.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Please join us for a gathering of</h2>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>family, friends, and food</h1>
+        <h2>to celebrate Thanksgiving at three o'clock</h2>
+        <h2>20 Roos Road, East Sandwich, MA</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Nav = () => {
+    return (
+        <div>
+            <h2>THANKSGIVING</h2>
+            <div>
+                <h3>PICK A</h3>
+                <h3>DISH</h3>
+                <h3>SEAT</h3>
+            </div>
+        </div>
+    )
+}
