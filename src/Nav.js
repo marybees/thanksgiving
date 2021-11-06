ad0fb7b1bc61d4bb259794e4f8d1ad1889dcee7b
Mary Baker <meb601@gmail.com>
2021-11-06 17:29:58 -0400

    Update Nav to word as guest list instead of seat

--- src/Nav.js
+++ src/Nav.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <div className="nav-body">
-            <Link style={{ textDecoration: "none", color: "white" }} to="/"><h2>Thanksgiving</h2></Link>
-            <div className="nav-body-half">
-                <h3>PICK A</h3>
-                <Link style={{ textDecoration: "none", color: "white" }} to="/dish"><h3 className="link">DISH</h3></Link>
-                <Link style={{ textDecoration: "none", color: "white" }} to="/seat"><h3 className="link">SEAT</h3></Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="nav-body">
+      <div className="nav-body-half">
+        <Link
+          style={{
+            textDecoration: "none",
+            color: "white",
+            marginRight: "1rem",
+          }}
+          to="/"
+        >
+          <h2>Thanksgiving</h2>
+        </Link>
+      </div>
+      <div className="nav-body-half">
+        <Link style={{ textDecoration: "none", color: "white" }} to="/dish">
+          <h3 className="link">PICK A DISH</h3>
+        </Link>
+        <Link style={{ textDecoration: "none", color: "white" }} to="/seat">
+          <h3 className="link">GUEST LIST</h3>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Nav
-
+export default Nav;
