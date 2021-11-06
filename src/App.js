af6185337c71c8cf2b4bd4acd329e3616413853c
Mary Baker <meb601@gmail.com>
2021-11-06 17:31:13 -0400

    Move Nav and Footer into root so it shows up on every page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Nav from "./Nav";
+import Footer from "./Footer";
 import Home from "./Home";
 import Seat from "./Seat";
 import Dish from "./Dish";
@@ -9,11 +11,13 @@ const App = () => {
   return (
     <div>
       <Router>
+      <Nav />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/seat" element={<Seat />}></Route>
           <Route path="/dish" element={<Dish />}></Route>
         </Routes>
+        <Footer />
       </Router>
     </div>
   )
