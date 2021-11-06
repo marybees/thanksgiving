164751ae95add23318c4e6e9824a37e58e8d2bee
Mary Baker <meb601@gmail.com>
2021-11-06 08:35:36 -0400

    Update App to include routes and move landing page into new Home component

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,15 @@
-import logo from './turkey.png';
+import React from 'react';
 
-function App() {
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Please join us for a gathering of</h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>family, friends, and food</h1>
-        <h2>to celebrate Thanksgiving at three o'clock</h2>
-        <h2>20 Roos Road, East Sandwich, MA</h2>
-      </header>
+    <div>
+      <h2>Please join us for a gathering of</h2>
+      <h1>family, friends, and food</h1>
+      <h2>to celebrate Thanksgiving at three o'clock</h2>
+      <h2>20 Roos Road, East Sandwich, MA</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default Home
+
